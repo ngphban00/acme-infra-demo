5d0d7a48096384e32d6d8e034b4487faad986604
An Nguyen <ngphban@gmail.com>
2026-06-28 11:29:50 +0000

    fix: use az cli auth for local execution

--- envs/dev/azure/main.tf
+++ envs/dev/azure/main.tf
@@ -24,9 +24,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  use_oidc        = true
-  client_id       = "9c315f24-a7d6-4e72-96de-6ad6c4f3abca"
-  tenant_id       = "9ae65231-1a32-4e61-876a-fe0b6857448d"
   subscription_id = "e3c6dd58-2eb8-4e7b-957f-6a4945309c10"
 }
 
