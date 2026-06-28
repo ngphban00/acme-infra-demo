5c84144783f2a440185529d656fc9bc66df655f0
An Nguyen <ngphban@gmail.com>
2026-06-28 11:19:04 +0000

    fix: hardcode non-secret azure ids in provider for demo

--- envs/dev/azure/main.tf
+++ envs/dev/azure/main.tf
@@ -25,9 +25,9 @@ terraform {
 provider "azurerm" {
   features {}
   use_oidc        = true
-  client_id       = var.arm_client_id
-  tenant_id       = var.arm_tenant_id
-  subscription_id = var.arm_subscription_id
+  client_id       = "9c315f24-a7d6-4e72-96de-6ad6c4f3abca"
+  tenant_id       = "9ae65231-1a32-4e61-876a-fe0b6857448d"
+  subscription_id = "e3c6dd58-2eb8-4e7b-957f-6a4945309c10"
 }
 
 module "order_portal" {
